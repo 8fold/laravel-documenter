65ace1a2dc9734b74f5b26361a402633e6e0d117
Josh Bruce <josh@8foldproductivity.com>
2017-07-01 16:06:48 -0400

    Update routes logic and setup

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,38 +2,12 @@
 
 $controller = Eightfold\Documenter\Controllers\ProjectsController::class;
 
-// Route::get('/', function () {
-//     return view('documenter::welcome');
-// });
-
-Route::domain('developer.'.env('APP_DOMAIN'))->group(function () use ($controller) {
+Route::domain('developer.'.env('DOCUMENTER_DOMAIN'))->group(function () use ($controller) {
     Route::get('/', $controller.'@index');
     Route::get('/{project}', $controller.'@viewProjectOverview');
     Route::get('/{project}/{version}', $controller.'@viewProjectVersion');
-
-
-
-
-
-    // Route::get('/{project}/{version}/traits/{trait}', $controller.'@viewTrait');
-    // Route::get('/{project}/{version}/interfaces/{trait}', $controller.'@viewInterface');
     Route::get('/{project}/{version}/{something}/methods/{method}', $controller.'@viewMethod')->where('something', '.*');
     Route::get('/{project}/{version}/{something}/properties/{method}', $controller.'@viewProperty')->where('something', '.*');
     Route::get('/{project}/{version}/{something}', $controller.'@viewSomething')
         ->where('something', '.*');
 });
-
-// Route::group([
-//         'prefix' => 'documentation.'.env(APP_DOMAIN)
-//     ], function() use ($controller) {
-//     Route::get('/', $controller.'@index');
-//     Route::get('/{project}', $controller.'@viewProjectOverview');
-//     Route::get('/{project}/{version}', $controller.'@viewProjectVersion');
-//     Route::get('/{project}/{version}/traits/{trait}', $controller.'@viewTrait');
-//     Route::get('/{project}/{version}/interfaces/{trait}', $controller.'@viewInterface');
-//     Route::get('/{project}/{version}/{something}/methods/{method}', $controller.'@viewMethod')->where('something', '.*');
-//     Route::get('/{project}/{version}/{something}/properties/{method}', $controller.'@viewProperty')->where('something', '.*');
-//     Route::get('/{project}/{version}/{something}', $controller.'@viewSomething')
-//         ->where('something', '.*');
-// });
-
